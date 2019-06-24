179530fa3567187536d9d7ebd12d44aab55f9450
Yuri Albuquerque <yuridenommus@gmail.com>
2019-06-24 11:50:34 -0300

    Refactoring some safety approaches.

--- src/lib.rs
+++ src/lib.rs
@@ -10,11 +10,11 @@ use libc::c_void;
 use bigint::uint::U256;
 use byteorder::{BigEndian, ByteOrder};
 
-use ffi::{randomx_flags, randomx_calculate_hash, randomx_vm};
+use ffi::{randomx_calculate_hash};
 
-pub use types::RxState;
+pub use types::{RxState, RxVM};
 
-pub fn calculate(vm: *mut randomx_vm, input: &mut [u8], nonce: u64) -> U256 {
+pub fn calculate(vm: &RxVM, input: &mut [u8], nonce: u64) -> U256 {
 	let mut result: [u8; 32] = [0; 32];
 	let input_size = input.len();
 
@@ -34,7 +34,7 @@ pub fn calculate(vm: *mut randomx_vm, input: &mut [u8], nonce: u64) -> U256 {
 
 	unsafe {
 		randomx_calculate_hash(
-			vm,
+			vm.vm,
 			input.as_ptr() as *const c_void,
 			input_size,
 			result.as_mut_ptr() as *mut c_void);
@@ -44,22 +44,20 @@ pub fn calculate(vm: *mut randomx_vm, input: &mut [u8], nonce: u64) -> U256 {
 } 
 
 pub fn slow_hash(state: &mut RxState, data: &[u8], seed: &[u8; 32]) -> U256 {
-	let vm = unsafe {
-		let cache = state.init_cache(seed, false).expect("seed no initialized");
-		state.create_vm().expect("vm no initialized")
+	let vm = {
+		state.init_cache(seed, false).expect("seed not initialized");
+		state.create_vm().expect("vm not initialized")
 	};
 
 	let hash_target = unsafe {
 		let mut hash: [u8; 32] = [0; 32];
 
 		ffi::randomx_calculate_hash(
-			vm,
+			vm.vm,
 			data.as_ptr() as *const c_void,
 			data.len(),
 			hash.as_mut_ptr() as *mut c_void,
 		);
-
-		ffi::randomx_destroy_vm(vm);
 
 		hash.into()
 	};
@@ -70,6 +68,7 @@ pub fn slow_hash(state: &mut RxState, data: &[u8], seed: &[u8; 32]) -> U256 {
 #[cfg(test)]
 mod test {
 	use super::*;
+    use super::utils::*;
 
 	#[test]
 	fn test_verify() {
